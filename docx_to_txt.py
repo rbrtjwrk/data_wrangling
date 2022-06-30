943a611e0281d13a372a7e557260588b8bde4951
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2022-06-30 11:23:51 +0200

    Update docx_to_txt.py

--- docx_to_txt.py
+++ docx_to_txt.py
@@ -2,24 +2,26 @@ import docx2txt
 import glob
 
 
-# process docx to txt file
-
 def docx_to_txt(file):
+    """Processes a .docx file to a .txt file.
+    """
     txt=docx2txt.process(file)
     txt=txt.replace("\n", "").replace("\t","")
     return txt
 
 
-# writes txt file in chosen directory
 
 def write_txt(directory, file_name, text):
+    """Writes a .txt file in a chosen directory.
+    """
     with open(directory+file_name+".txt", 'w') as f:  
         f.write(text)
 
         
-# process all docx files in given directory to txt files
 
 def process_docx_to_txt(directory):
+    """Processes all .docx files in given directory to .txt files.
+    """
     docx_files=[file for file in glob.iglob(directory+"*docx")]
     for file in docx_files:
         text=docx_to_txt(file)
